728d23ac61aa7a5d09cc5962e17dc4dbdfc0c993
jimmy annes <jimmyannes98@gmail.com>
2014-12-03 13:41:16 -0800

    platformer milestone 14

--- core/src/com/annesj/platformer/model/Player.java
+++ core/src/com/annesj/platformer/model/Player.java
@@ -2,14 +2,23 @@ package com.annesj.platformer.model;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Vector2;
 
 public class Player {
     public Vector2 position;//a point for x and y positioning
-    public Texture spriteSheet;//
+    public Texture spriteSheet;//creates object for spritesheet
 
     public Player() {
         position = new Vector2(0, 0);//positions the player in the bottom right corner of the screen
         spriteSheet = new Texture(Gdx.files.internal("img/aliens.png"));//allows us to access the image in assests folder and changing it into a texture
+    }
+
+    public void draw(Batch spriteBatch){
+        spriteBatch.draw(spriteSheet, 0, 0, 70, 100);
+    }
+
+    public void update(float deltaTime){
+
     }
 }
--- core/src/com/annesj/platformer/view/GameScreen.java
+++ core/src/com/annesj/platformer/view/GameScreen.java
@@ -1,55 +1,55 @@
 package com.annesj.platformer.view;
 
+import com.annesj.platformer.model.Player;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class GameScreen implements Screen {
-    public TiledMap map;
-    //sets the object for the map
+    public TiledMap map;//sets the object for the map
     public OrthogonalTiledMapRenderer renderer;
     public OrthographicCamera camera;
 
-    public GameScreen() {
-        map = new TmxMapLoader() .load("map/map.tmx");
-        //the connecter to the online sit that loads the map
-        renderer = new OrthogonalTiledMapRenderer(map, 1/70f);
-        //to call the class to render the map
+    public SpriteBatch spriteBatch;
+    public Player player;
 
-        float width = Gdx.graphics.getWidth();
-        //finds width of window for use in the game map
-        float height = Gdx.graphics.getHeight();
-        //fits thh game map to the window
-        camera = new OrthographicCamera(14f, 14f * (height / width));
-        //to initialize the camera
-        camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f,0);
-        //sets the view to half of what it is and center it
+    public GameScreen() {
+        map = new TmxMapLoader() .load("map/map.tmx");//the connecter to the online sit that loads the map
+        renderer = new OrthogonalTiledMapRenderer(map, 1/70f);//to call the class to render the map
+
+        float width = Gdx.graphics.getWidth();//finds width of window for use in the game map
+        float height = Gdx.graphics.getHeight();//fits thh game map to the window
+        camera = new OrthographicCamera(14f, 14f * (height / width));//to initialize the camera
+        camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f,0);//sets the view to half of what it is and center it
+
+        spriteBatch = new SpriteBatch();
+        player = new Player();
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1f,.52f,.9686274f,1f);
-        //sets the color of your clear
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        //clears the screen using the color youve chosen
-        camera.update();
-        //updating the camera
-        renderer.setView(camera);
-        //sets where the camera is
-        renderer.render();
-        //sets render to start
+        Gdx.gl.glClearColor(1f,.52f,.9686274f,1f);//sets the color of your clear
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);//clears the screen using the color youve chosen
+
+        camera.update();//updating the camera
+        renderer.setView(camera);//sets where the camera is
+        renderer.render();//sets render to start
+
+        spriteBatch.begin();
+        player.draw(spriteBatch);
+        spriteBatch.end();
     }
 
     @Override
     public void resize(int width, int height) {
-    camera.viewportWidth = 14f;
-    camera.viewportHeight = 14f * height / width;
-    camera.update();
-    //sets the weight and hidth to the window when resizing
+        camera.viewportWidth = 14f;
+        camera.viewportHeight = 14f * height / width;
+        camera.update();//sets the weight and hidth to the window when resizing
     }
 
     @Override
