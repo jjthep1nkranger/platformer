1bd45cb0db942e5bfe615eef23bb267ad7fd5c46
jimmy annes <jimmyannes98@gmail.com>
2014-12-08 14:06:17 -0800

    no message

--- core/src/com/annesj/platformer/model/Player.java
+++ core/src/com/annesj/platformer/model/Player.java
@@ -3,21 +3,46 @@ package com.annesj.platformer.model;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 public class Player {
     public Vector2 position;//a point for x and y positioning
     public Texture spriteSheet;//creates object for spritesheet
 
+    public TextureRegion[] spriteFrames;//
+
     public Player() {
-        position = new Vector2(0, 0);//positions the player in the bottom right corner of the screen
+        position = new Vector2(2, 2);//positions the player in the bottom right corner of the screen
         spriteSheet = new Texture(Gdx.files.internal("img/aliens.png"));//allows us to access the image in assests folder and changing it into a texture
+
+        TextureRegion[][] spriteSheetFrames = TextureRegion.split(spriteSheet, 70,100);//it splits the regions up and sets them in the 2d array
+
+        int counter = 0;//
+        for(int row = 0; row < spriteSheetFrames.length; row++){
+            for(int column = 0; column < spriteSheetFrames[row].length; column++){
+                counter++;//adds one to the counter each time
+            }
+        }
+
+        spriteFrames = new TextureRegion[counter];//initializes the value so we can store the sprites
+
+        counter = 0;
+        for(TextureRegion[] row : spriteSheetFrames){
+            for(TextureRegion sprite : row){
+                spriteFrames[counter++] = sprite;
+            }
+
+        }
+
     }
 
     public void draw(Batch spriteBatch){
-        spriteBatch.draw(spriteSheet, 0, 0, 70, 100);}//the function for drawing the player
+        spriteBatch.draw(spriteFrames[11], position.x, position.y, 70 * (1/70f), 100 * (1/70f));}//the function for drawing the player
+
 
     public void update(float deltaTime){
+        position.x+= deltaTime;//moves the player to the right slowly
 
     }
 }
--- core/src/com/annesj/platformer/view/GameScreen.java
+++ core/src/com/annesj/platformer/view/GameScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
@@ -15,7 +16,7 @@ public class GameScreen implements Screen {
     public OrthogonalTiledMapRenderer renderer;
     public OrthographicCamera camera;
 
-    public SpriteBatch spriteBatch;
+    public Batch spriteBatch;
     public Player player;
 
     public GameScreen() {
@@ -27,19 +28,20 @@ public class GameScreen implements Screen {
         camera = new OrthographicCamera(14f, 14f * (height / width));//to initialize the camera
         camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f,0);//sets the view to half of what it is and center it
 
-        spriteBatch = new SpriteBatch();//creates the spritebatch object
+        spriteBatch = renderer.getSpriteBatch();//gets acsess to the spritebatch object
         player = new Player();//creates the player object
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1f,.52f,.9686274f,1f);//sets the color of your clear
+        Gdx.gl.glClearColor(.3019607843f,1f,.34901960784f,1f);//sets the color of your clear
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);//clears the screen using the color youve chosen
 
         camera.update();//updating the camera
         renderer.setView(camera);//sets where the camera is
         renderer.render();//sets render to start
 
+        player.update(delta);
         spriteBatch.begin();//begins the spritebatch
         player.draw(spriteBatch);//draws the player in the window
         spriteBatch.end();//ends the spritebatch
